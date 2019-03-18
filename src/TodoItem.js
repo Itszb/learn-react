b0decdf8ccdf080f5c7a9e105a142956f96e4f70
zb <zb>
2019-03-18 18:01:29 +0800

    1

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 
+import PropTypes from 'prop-types';
+//当一个组件的props和state发生改变的时候 他自己的render函数就会重新执行
+//就会用新的数据去渲染页面 所以你的数据就会被渲染出来了
+
+// /虚拟dom
+
 class TodoItem extends Component {
   constructor(props) {
     super(props);
     this.handlerDelete = this.handlerDelete.bind(this);
+  }
+  //是否要更新呢?
+  shouldComponentUpdate(nextProps,nextState){
+    if(nextProps.content!==this.props.content){
+      return true
+    }else{
+      return false;
+    }
+   
   }
   render() { 
     return <div onClick={this.handlerDelete}>{this.props.content}</div>;
@@ -11,5 +26,14 @@ class TodoItem extends Component {
   handlerDelete() {
     this.props.deleteItem(this.props.index);
   }
+}
+// 类型做一个校验
+TodoItem.propTypes={
+  content:PropTypes.oneOfType([PropTypes.number,PropTypes.string]).isRequired,
+  handlerDelete:PropTypes.func,
+  index:PropTypes.number
+}
+TodoItem.defaultProps={
+  content:'hello world'
 }
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,16 @@ import React, { Component, Fragment } from "react";
 import TodoItem from "./TodoItem";
 import "./style.css";
 
+
+
+
+// 什么是生命周期函数
+// 在某一个时刻组件会自动调用执行的函数
+
+
+// 对于一个组件来说    他的父组件的render函数执行了  他的render函数就会执行
+
+
 class TodoList extends Component {
   constructor(props) {
     super(props);
@@ -10,17 +20,28 @@ class TodoList extends Component {
       list: ["english", "react"]
     };
   }
+  //组件更新之前 会执行
+  shouldComponentUpdate(){
+    return true;
+  }
+  componentWillUpdate(){
+
+  }
   handleInputChange(e) {
-    const value = e.target.value;
+   //使用ref
+    const value = this.input.value;
     this.setState(() => ({
       inputValue: value
     }));
   }
   handleButtonClick() {
+    //setState是一个异步函数
     this.setState(prevState => ({
       list: [...prevState.list, prevState.inputValue],
       inputValue: ""
-    }));
+    }),()=>{
+
+    });
   }
   handlerDelete(index) {
     const list = [...this.state.list];
@@ -39,6 +60,7 @@ class TodoList extends Component {
           type="text"
           value={this.state.inputValue}
           onChange={this.handleInputChange.bind(this)}
+          ref={(input)=>{this.input=input}}
         />
         <button onClick={this.handleButtonClick.bind(this)}>add</button>
         <ul>
